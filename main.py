e1a2da26e3ec45241bb9b60c7a3381af5c7b36d7
Meeple397 <islaymaker397@gmail.com>
2025-03-05 14:16:59 -0500

    F: added fsp

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get(): #must be begining of loop
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
-        pygame.display.flip() #must be end of loop
+            
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
