215e6eb70192b1ffb18549c70c6aa10805ac3ef5
Meeple397 <islaymaker397@gmail.com>
2025-03-05 14:07:22 -0500

    E: Wrote the initial 'game' loop to make a screen

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get(): #must be begining of loop
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(000)
+        pygame.display.flip() #must be end of loop
     
 if __name__ == "__main__":
     main()
