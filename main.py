0570e0f0f2927dd1384ffe71da85d83d5475b34b
Meeple397 <islaymaker397@gmail.com>
2025-03-06 12:34:30 -0500

    I: Added asteroids, player shooting, and killing/splitting of asteroids.  IE, finished the game.

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from bullet import Shot
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
-    
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
-    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -23,10 +34,22 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+            
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
+
         for obj in drawable:
             obj.draw(screen)
-        
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
